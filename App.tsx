f7bb9a3a6c23910d674adc55bec38914b54a44df
Natanael Lima <taelima1997@gmail.com>
2021-11-30 23:40:28 -0300

    add LoadingIndicator animation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { MotiView } from "@motify/components";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
+const LoadingIndicator = ({ size }: { size: number }) => {
+  return (
+    <MotiView
+      from={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        borderWidth : 0,
+        shadowOpacity: 0.5,
+      }}
+      animate={{
+        width: size + 20,
+        height: size + 20,
+        borderRadius: (size + 20) / 2,
+        borderWidth: size / 10,
+        shadowOpacity: 1,
+      }}
+      transition={{
+        type: "timing",
+        duration: 1000,
+        loop : true,
+      }}
+      style={{
+        borderColor: "#fff",
+        shadowColor: "#fff",
+        shadowOffset: { width: 0, height: 0 },
+        
+        shadowRadius: 10,
+      }}
+    ></MotiView>
+  );
+};
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoadingIndicator size={80} />
     </View>
   );
 }
@@ -14,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#111",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
